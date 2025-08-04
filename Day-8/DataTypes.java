e574c9f80d24e9bbe13b8360ac0544b4aba91d3e
firrushaik <shaikfirdose7799@gmail.com>
2025-08-04 20:29:22 +0530

    Data types

---
+++ Day-8/DataTypes.java
@@ -0,0 +1,30 @@
+Data type : Data types refers to the different sizes and values that can be stored in the variable.
+
+Data type refers to the which type of data or value to be stored in the variable . 
+According to that data type it will allocate the size of the memory in the stack area. 
+
+There are 2 types of data types in java :
+
+Primitive data type 
+Non primitive data type
+
+Primitive data type : These are the most basic data types, directly storing the values in the memory.
+They are pre defined by the language and not objects.
+In java , 8 primitive data types:
+
+byte: 8-bit signed integer.
+short: 16-bit signed integer.
+int: 32-bit signed integer (most commonly used for whole numbers).
+long: 64-bit signed integer.
+float: 32-bit single-precision floating-point number.
+double: 64-bit double-precision floating-point number (most commonly used for decimal numbers).
+char: 16-bit Unicode character.
+boolean: Stores true or false values.  
+
+
+Non primitive data types : These datthe actual data a types do not directly store the data but store references(memory addresses) to objects that hold the actual data in heap memory.
+
+Types : String
+Arrays
+Classes
+Interfaces
