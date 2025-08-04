198be8c63dd1153cd89b0379dcc2e590815e4771
firrushaik <shaikfirdose7799@gmail.com>
2025-08-04 18:34:10 +0530

    Types of variables examples

--- Day-8/Types-of-variables.java
+++ Day-8/Types-of-variables.java
@@ -54,3 +54,18 @@ System.out.println("My name is:" + b1.name);
 
 output :
 My name is:firru
+
+Some other example :
+
+class InstanceEx
+{
+int age ;
+public static void main(String[] args)
+{
+InstanceEx b1 = new InstanceEx(); // We should not assign a value to variable , It will take the default value as 0
+System.out.println("My age is:" + b1.age);
+}
+}
+
+output :
+My age is: 0
