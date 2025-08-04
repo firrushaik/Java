a1abecfeb7836799fb9445a00dbcec61f5be1b5d
firrushaik <shaikfirdose7799@gmail.com>
2025-08-04 18:12:20 +0530

    Types of variables examples

---
+++ Day-8/Types-of-variables.java
@@ -0,0 +1,56 @@
+Variables are containers used to store the data.
+
+Types of variables :
+
+Local variable 
+Static variable
+Instance variables
+
+Local variables : Local variables are declared within a method or block.
+Example :
+
+class LocalEx
+{
+public static void main(String[] args)
+{
+int age = 24;
+System.out.println("age is:" + age);
+}
+}
+
+out put : 
+age is: 24
+
+Static variable : Static variables are declared with a static keyword within a class but outside a method.
+It is also called as Class variables.
+
+Example :
+
+class StaticEx
+{
+static int age = 20;
+public static void main(String[] args)
+{
+System.out.println("age is:" + age);
+}
+}
+
+output : age is :20
+
+Instance variables : Instance variables are declared within a class but outside a method or block.
+ 
+ Example :
+
+ class InstanceEx
+{
+String name ;
+public static void main(String[] args)
+{
+InstanceEx b1 = new InstanceEx();
+b1.name = "firru";
+System.out.println("My name is:" + b1.name);
+}
+} 
+
+output :
+My name is:firru
