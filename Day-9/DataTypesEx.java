23b8a679685a0da58570301fcc51ce92a2abb443
firrushaik <shaikfirdose7799@gmail.com>
2025-08-05 23:52:12 +0530

    Data types

---
+++ Day-9/DataTypesEx.java
@@ -0,0 +1,34 @@
+Data type : Data type defines the type and size of a value that a variable can hold.
+Java data types are mainly devided into two categories. They are listed below:
+Primitive data type
+Non primitive data type
+
+Primitive data type : These are the basic-built-in data types that are store simple values directly. 
+Java eight primitive data types:
+   boolean
+   char
+   byte
+   short
+   int
+   long 
+
+
+Type                Description                            Memory allocation           Range           Default values
+
+boolean         It stores True or False values               1 byte(8 bits)                                false
+
+char            It stores the single character/             2 bytes(16 bit)          0 to 65536            \u0000
+                letter or ASCII values
+
+byte            Stores whole numbers                        1 byte(8 bit)           +127 to -128             0
+
+short           Stores whole numbers                         2 bytes                +32657 to -32658         0
+         
+int            It stores the integer values                   4 bytes               2^32 -1 to -2^32         0
+
+long           It is used when an int is not large,           8 bytes               2^64 -1 to -2^64         0L
+               enough to hold a value
+
+float          Stores decimol numbers                          4 bytes                                       0.0f
+
+double         Stores decimol numbers                          8 bytes                                       0.0d
