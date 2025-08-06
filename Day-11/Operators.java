fce5a78b2161c9847651e72f62f5c462cfa58626
firrushaik <shaikfirdose7799@gmail.com>
2025-08-07 00:09:39 +0530

    Operators

---
+++ Day-11/Operators.java
@@ -0,0 +1,16 @@
+Operators : Operators are symbols which represents the particular action on variables or values.
+Operators operate the operand. Operand may be either variable or constant.
+       eg : a + b ;  // here + is operator , A and B are operands
+
+There are 8 types of operators in java :
+
+Arithematic operator
+Assignment operator
+An instance of an operator
+Bitwise operator
+Logical operator
+Relational operator
+Ternary operator
+Unary operator
+
+
