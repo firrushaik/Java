135d26e9fc062b19c1aa9d6c7f0cf14e618012e0
firrushaik <shaikfirdose7799@gmail.com>
2025-08-09 18:14:21 +0530

    Leap year or not

---
+++ Day-13/Leapyear.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Leapyear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the year: ");
+        int year = sc.nextInt();
+        if(year%4==0){
+            System.out.println(year + " Is leap year. ");
+        }
+        else{
+            System.out.println(year + " Is not a leap year. ");
+        }
+    }
+}
