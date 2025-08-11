58402f81681c83941b9554b5f4dc9cd1b65a5a36
firrushaik <shaikfirdose7799@gmail.com>
2025-08-11 16:42:41 +0530

    Print N odd  numbers

--- Day-15/OddNumbers1.java
+++ Day-15/OddNumbers1.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-public class primeNumbers {
+public class OddNumbers1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number: ");
         int num = sc.nextInt();
         for(int i=1;i<=num;i++)
         {
-            if(i%2==0) {
+            if(i%2!=0) {
                 System.out.println(i );
             }
         }
