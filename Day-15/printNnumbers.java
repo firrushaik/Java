67510eecc358e803c72267d2e4eed7b3b5ac5bc8
firrushaik <shaikfirdose7799@gmail.com>
2025-08-11 16:31:15 +0530

    Print N prime  numbers

--- Day-15/printNnumbers.java
+++ Day-15/printNnumbers.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
-public class printNnumbers{
+public class primeNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number: ");
-        int n = sc.nextInt();
-        for( int i=0;i<n;i++)
+        int num = sc.nextInt();
+        for(int i=1;i<=num;i++)
         {
-            System.out.println(i);
+            if(i%2==0) {
+                System.out.println(i );
+            }
         }
-
     }
 }
