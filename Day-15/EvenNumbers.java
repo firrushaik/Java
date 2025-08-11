47c165589084b8ede1fd35d84fbffec97eab6151
firrushaik <shaikfirdose7799@gmail.com>
2025-08-11 16:33:28 +0530

    Print N even  numbers

--- Day-15/EvenNumbers.java
+++ Day-15/EvenNumbers.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class primeNumbers {
+public class EvenNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number: ");
