0a6f5e71e23d26520f99a0267e6a4f247ac2ea61
firrushaik <shaikfirdose7799@gmail.com>
2025-08-12 19:24:01 +0530

    patterns examples

---
+++ Day-16/Patterns2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Patterns2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        int num = sc.nextInt();
+        for(int num1 = 1; num1<=num ; num1++){
+            for(int num2 = 1 ; num2<=num1 ; num2++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+/* for n = 4
+
+* * * *
+* * * *
+* * * *
+* * * *
+
+*/
+
