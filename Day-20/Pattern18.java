85cf09d01397f69318720935f4ec5680d0f97639
firrushaik <shaikfirdose7799@gmail.com>
2025-08-16 16:18:38 +0530

    pattern 18

---
+++ Day-20/Pattern18.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Pattern18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n: ");
+        int n = sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print((char)('A'+k-1));
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+/* OUTPUT:
+
+n=4
+
+   A
+  AB
+ ABC
+ABCD 
+
+*/
