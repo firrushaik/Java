877971938b09646a611ecaa76b48da04fed45ae5
firrushaik <shaikfirdose7799@gmail.com>
2025-08-20 23:42:50 +0530

    Cube of N

---
+++ Day-25/CubeOfN.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class cubeOfN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter n: ");
+        int n = sc.nextInt();
+        System.out.println(n*n*n);
+    }
+}
+
+
+/*
+
+Enter n:
+5
+125
+
+Enter n:
+6
+216
+
+*/
