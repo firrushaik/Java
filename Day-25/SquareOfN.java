60e881e8afc1dd80962a5d5f540014f0c679e6db
firrushaik <shaikfirdose7799@gmail.com>
2025-08-20 23:36:11 +0530

    Square of N

---
+++ Day-25/SquareOfN.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class squareOfN {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter n ");
+        int n=sc.nextInt();
+        System.out.println(n*n);
+    }
+}
+
+/* Enter n
+
+9
+81
+
+7
+49
+
+*/
