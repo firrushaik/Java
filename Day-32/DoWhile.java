86ab786496bd8945d5c383139f738e11cfeceeb5
firrushaik <shaikfirdose7799@gmail.com>
2025-08-26 23:01:56 +0530

    Do while example

---
+++ Day-32/DoWhile.java
@@ -0,0 +1,18 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int c = 1;
+
+        do{
+            System.out.println("hello" + c);
+            c++;
+        }
+        while(c < 5);
+    }
+}
+
+/*
+hello1
+hello2
+hello3
+hello4
+*/
