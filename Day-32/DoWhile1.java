6cab4eb8ad07a19cc0124b9c8b570f4546cf0172
firrushaik <shaikfirdose7799@gmail.com>
2025-08-26 23:10:42 +0530

    Do while example1

---
+++ Day-32/DoWhile1.java
@@ -0,0 +1,27 @@
+
+
+import java.util.Scanner;
+
+// Program to find the sum of natural numbers from 1 to 100.
+
+ public class DoWhile1
+ {
+    public static void main(String[] args) {
+
+        int i = 1, n = 5;
+
+        // do...while loop from 1 to 5
+        do {
+            System.out.println(i);
+            i++;
+        } while(i <= n);
+    }
+}
+
+/*
+Hello and welcome!i = 1
+i = 2
+i = 3
+i = 4
+i = 5
+*/
