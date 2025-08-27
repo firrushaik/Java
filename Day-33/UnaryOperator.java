bd57b57147b3011974d94c98b393c71b87320305
firrushaik <shaikfirdose7799@gmail.com>
2025-08-27 21:55:54 +0530

    Unary  Operator Example

---
+++ Day-33/UnaryOperator.java
@@ -0,0 +1,9 @@
+public class UnaryOperatorEx {
+    public static void main(String[] args) {
+        int count = 10;
+        System.out.println(++count);   //11
+        System.out.println(--count);   //10
+        System.out.println(count++);   //10
+        System.out.println(count--);   //11
+    }
+}
