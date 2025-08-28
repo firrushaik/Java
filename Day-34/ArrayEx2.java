db908dd5139a96b8d0ad09ec1f84e613cd08bcdf
firrushaik <shaikfirdose7799@gmail.com>
2025-08-28 18:33:25 +0530

    Array boolean Example

---
+++ Day-34/ArrayEx2.java
@@ -0,0 +1,9 @@
+public class ArrayEx2 {
+    public static void main(String[] args) {
+        boolean arr[] = new boolean[2]; // size of the array is 2
+        System.out.println(arr[1]); // index starts with 0 ans ends with 1(size-1) , (2-1=1)
+    }
+}
+
+/*
+false // boolean default value is false */
