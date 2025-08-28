d65aa175cd21711da97112d1a260dd7aa15f10da
firrushaik <shaikfirdose7799@gmail.com>
2025-08-28 14:40:18 +0530

    Array Example

---
+++ Day-34/ArrEx.java
@@ -0,0 +1,10 @@
+public class ArrayEx {
+    public static void main(String[] args) {
+        int arr[] = new int[6];       // Size of the array is 6 
+        System.out.println(arr[4]);  // Here 4 represents the index of the array
+    }
+}
+
+/* output is : 0 
+ 
+Why because int default value is 0. size of the array is 6 . the index of array should starts with 0 and ends with 5(size-1).
