40136c962179e29d7273b40173898e7c07a11873
firrushaik <shaikfirdose7799@gmail.com>
2025-08-29 14:50:33 +0530

    Array example 5

---
+++ Day-35/ArrayEx5.java
@@ -0,0 +1,34 @@
+public class ArrayEx5 {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        
+        // It is our wish to print which index should be first,last etc. 
+        System.out.println(arr[1]); 
+        System.out.println(arr[4]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[0]);
+        System.out.println(arr[7]);
+        System.out.println(arr[6]);
+        System.out.println(arr[8]);
+        System.out.println(arr[9]);
+        System.out.println(arr[5]);
+
+    }
+}
+
+/*
+
+20
+50
+30
+40
+10
+80
+70
+90
+100
+60
+
+*/
