f7778fef2612e617551e43d3fffcc30d880e8425
firrushaik <shaikfirdose7799@gmail.com>
2025-08-29 14:36:18 +0530

    Array example 3

---
+++ Day-35/ArrEx3.java
@@ -0,0 +1,25 @@
+public class ArrayEx3 {
+    public static void main(String[] args) {
+
+        int arr[] = new int[6];       // here 6 represents the size of the array
+       
+        System.out.println(arr[0]);  // index starts with 0 and ends with (size-1) i.e, 5.
+        System.out.println(arr[1]);  // here inside the print statements arr represents the array name and 1 is index of the array we have to print
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+        System.out.println(arr[5]);
+    }
+}
+
+
+/*
+
+0      
+0
+0 
+0
+0
+0
+
+*/
