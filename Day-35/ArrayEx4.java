06e310dfd6598a5fdcc4ec59f944267ca867cf9a
firrushaik <shaikfirdose7799@gmail.com>
2025-08-29 14:44:31 +0530

    Array example 4

---
+++ Day-35/ArrayEx4.java
@@ -0,0 +1,57 @@
+public class ArrayEx4 {
+    public static void main(String[] args) {
+
+        int[] arr = {10,20,30,40,50,60,70,80,90,100};
+
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+        System.out.println(arr[5]);
+        System.out.println(arr[6]);
+        System.out.println(arr[7]);
+        System.out.println(arr[8]);
+        System.out.println(arr[9]);
+
+
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+        System.out.println(arr[5]);
+        System.out.println(arr[6]);
+        System.out.println(arr[7]);
+        System.out.println(arr[8]);
+        System.out.println(arr[9]);
+
+
+    }
+}
+
+
+/*
+
+10
+20
+30
+40
+50
+60
+70
+80
+90
+100
+10
+20
+30
+40
+50
+60
+70
+80
+90
+100
+
+*/
