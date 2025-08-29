ca3b922fe6c49270ab6d6e0b29fde937164dc72e
firrushaik <shaikfirdose7799@gmail.com>
2025-08-29 19:19:54 +0530

    Reading Array Elements

---
+++ Day-35/ReadingArrayElements.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class ReadingArrayElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array");
+        int size = sc.nextInt();
+        
+        System.out.println("Enter " + size + " array elements");
+        int[] arr = new int[size];
+
+        // reading array data from the user
+        for(int i=0;i<=size-1;i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        // print array elements
+        for(int i = 0;i<=size-1;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
