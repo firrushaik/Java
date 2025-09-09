5b32265bc7a51608950e971fff26f053a654ccb3
firrushaik <shaikfirdose7799@gmail.com>
2025-09-09 13:57:35 +0530

    Make Pi

---
+++ CodingBat/makePi.java
@@ -0,0 +1,21 @@
+Q) Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+
+makePi() → [3, 1, 4]
+
+
+Ans:
+
+public int[] makePi() {
+  int[] makePi = new int[3];
+    makePi[0]=3;
+    makePi[1]=1;
+    makePi[2]=4;
+  return makePi;
+}
+
+
+output:
+
+makePi() → [3, 1, 4]	[3, 1, 4]	OK	
+
