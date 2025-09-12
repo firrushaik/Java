1acf4ffb0d5d898b2abde7dd5710b01dd2bbc8b3
firrushaik <shaikfirdose7799@gmail.com>
2025-09-12 17:19:10 +0530

    Bubble Sort Program

---
+++ Day-43/BubbleSort.java
@@ -0,0 +1,51 @@
+package ArrayPackage;
+
+import java.util.Scanner;
+public class BubbleSort {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+        System.out.println("Enter the " + size + " array elements: ");
+
+        for(int i = 0; i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i = 0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+
+        //print the array
+        for(int i = 0;i<arr.length;i++){
+            System.out.println(arr[i]+ " ");
+        }
+    }
+}
+
+
+OutPut:
+
+Enter the size of the array: 
+6
+Enter the 6 array elements: 
+7
+8
+4
+9
+5
+2
+2 
+4 
+5 
+7 
+8 
+9 
+
