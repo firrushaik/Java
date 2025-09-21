e49d10991acefd684fc4e27e1dde491f5ad4dc85
firrushaik <shaikfirdose7799@gmail.com>
2025-09-21 18:24:32 +0530

    String Example Uploaded

---
+++ Day-51/Strings.java
@@ -0,0 +1,24 @@
+package Strings;
+
+import java.util.Scanner;
+public class StringEx {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //In string data type first letter is capital because it is class name as well as data type
+        String name = "Firdose";
+        String name1 = "Shaik";
+
+        //print data
+        System.out.println(name);
+        System.out.println(name1);
+    }
+
+}
+
+
+
+output:
+
+Firdose
+Shaik
