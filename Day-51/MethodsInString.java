d3623cd04fadb7435051045dd5e63a7c463a8cf7
firrushaik <shaikfirdose7799@gmail.com>
2025-09-24 17:57:25 +0530

    String Methods

---
+++ Day-51/MethodsInString.java
@@ -0,0 +1,50 @@
+package Strings;
+
+public class concatMethod {
+    public static void main(String[] args) {
+
+        // concat()
+
+//        String s1 = "john";
+//        String s2 = "doe";
+//
+//        System.out.println(s1.concat(s2));
+
+
+        //contains()
+
+//        String s1 = "java is a programming language";
+
+//        System.out.println(s1.contains("is")); // true
+//        System.out.println(s1.contains("john")); // false
+
+        //startswith()
+
+//        String str = "java is a programming language";
+//        System.out.println(str.startsWith("java is")); //true
+
+        //endsWith()
+
+//        String str = "java is a programming language";
+//
+//        System.out.println(str.endsWith("language")); //true
+//
+//        System.out.println(str.endsWith("programming")); //false
+
+
+        //indexOf()
+
+//        String str = "java";
+//        System.out.println(str.indexOf('v')); //1
+//
+//        System.out.println(str.indexOf('o')); // -1
+
+
+
+        // substring()
+        
+        String str = "rock star";
+        System.out.println(str.substring(2,6)); //ck s
+
+    }
+}
