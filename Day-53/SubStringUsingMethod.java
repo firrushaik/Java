ae0ef779903ad9e9ba596e433f5028758b5d9255
firrushaik <shaikfirdose7799@gmail.com>
2025-09-28 19:14:10 +0530

    Sub string  using methods program uploaded

---
+++ Day-53/SubStringUsingMethod.java
@@ -0,0 +1,43 @@
+package Methods;
+
+import java.util.Scanner;
+public class SubStringUsingMethod {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the string: ");
+        String str = sc.nextLine();
+
+        allSubString(str);
+
+    }
+
+    static void allSubString(String str) {
+
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i; j < str.length(); j++) {
+                for (int k = i; k <= j; k++) {
+                    System.out.print(str.charAt(k) + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+
+
+
+output:
+
+Enter the string: 
+java
+j 
+j a 
+j a v 
+j a v a 
+a 
+a v 
+a v a 
+v 
+v a 
+a 
