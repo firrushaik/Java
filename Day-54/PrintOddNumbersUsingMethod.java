a62add7adebef6dc2c770b8be1896527e2910d6e
firrushaik <shaikfirdose7799@gmail.com>
2025-09-29 20:00:59 +0530

    Print odd numbers using method

--- Day-54/PrintOddNumbersUsingMethod.java
+++ Day-54/PrintOddNumbersUsingMethod.java
@@ -1,33 +1,32 @@
 package LoopUsingMethod;
 
 import java.util.Scanner;
-public class PrintEvenNumbersUsingMethod {
+
+public class PrintOddNumbersUsingMethod {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter number: ");
         int n = sc.nextInt();
 
-        evenNumbers(n);
+        oddNumbers(n);
     }
 
-    static void evenNumbers(int n) {
-        int i = 2;
+    static void oddNumbers(int n) {
+        int i = 1;
 
         while (i <= n) {
-                System.out.println(i + " ");
-                i +=2;
+            System.out.println(i + " ");
+            i +=2;
         }
 
     }
 }
 
-
 output:
 
 Enter number: 
 6
-
-2 
-4 
-6 
+1 
+3 
+5 
