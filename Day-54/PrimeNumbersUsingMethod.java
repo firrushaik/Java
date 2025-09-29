6678804db396ed675ef0b623b41b141d95678979
firrushaik <shaikfirdose7799@gmail.com>
2025-09-29 20:06:57 +0530

    Print prime numbers using method

---
+++ Day-54/PrimeNumbersUsingMethod.java
@@ -0,0 +1,37 @@
+package LoopUsingMethod;
+
+import java.util.Scanner;
+
+public class PrintNprimeNumbersUsingMethod {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n: ");
+        int n = sc.nextInt();
+
+        primeNumbers(n);
+
+    }
+
+    static void primeNumbers(int n){
+
+        int totalFactors = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                totalFactors++;
+            }
+        }
+        if (totalFactors == 2) {
+            System.out.println("Prime number");
+        } else {
+            System.out.println("Not prime number");
+        }
+    }
+}
+
+
+output:
+
+Enter n: 
+97
+Prime number
