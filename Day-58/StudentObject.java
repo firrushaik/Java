6f6cff7c8dafe62dd68b0f78541acb5e930621f9
firrushaik <shaikfirdose7799@gmail.com>
2025-10-06 20:33:27 +0530

    Book Object

---
+++ Day-58/StudentObject.java
@@ -0,0 +1,46 @@
+package OOPS;
+
+public class Student {
+    private String name;
+    private int fee;
+    private String course;
+
+    //setters and getters methods
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public void setFee(int fee){
+        this.fee=fee;
+    }
+
+    public void setCourse(String course){
+        this.course=course;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getFee(){
+        return fee;
+    }
+
+    public String getCourse(){
+        return course;
+    }
+
+    public void doHomeWork(){
+        System.out.println("Doing home work");
+    }
+}
+
+// private is access modifier
+//if we specify any variable with private access modifier
+//we can access that variable only within that class
+
+
+//setters -> assign value to variables
+//getters -> access values
+
+// this keyword -> it tell that variable belongs to current object
