bab61761e2a85276c8a317e63493783abfcc769b
firrushaik <shaikfirdose7799@gmail.com>
2025-10-13 19:47:06 +0530

    What is polymorphism

---
+++ Day-64/Polymorphism.java
@@ -0,0 +1,19 @@
+Polymorphism:
+
+The word polymorphism means having many forms, and it comes from the Greek words poly (many) and morph (forms), this means one entity can take many forms. In Java, polymorphism allows the same method or object to behave differently based on the context, specially on the project's actual runtime class.
+
+The word poly means "many" and morphism means "forms".
+
+One thing exixting in different forms is called "Polymorphism".
+
+Example:
+
+I am here acting like a interviwee.
+In schools I will be student for teachers.
+In home I'll be kid for my parents.
+
+Two types of polimorphism:
+
+1) compile time polymorphism(Method overloading)
+
+2) run time polymorphism(Method overriding)
