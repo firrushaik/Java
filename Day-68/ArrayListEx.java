16efa726367cd562bfb04d1e59b92563c5bb52e6
firrushaik <shaikfirdose7799@gmail.com>
2025-10-24 20:15:30 +0530

    Array list definition

---
+++ Day-68/ArrayListEx.java
@@ -0,0 +1,16 @@
+Array List:
+
+       The Java ArrayList class provides resizable-array and implements the List interface.
+Following are the important points about ArrayList −
+
+It implements all optional list operations and it also permits all elements, includes null.
+
+It provides methods to manipulate the size of the array that is used internally to store the list.
+
+The constant factor is low compared to that for the LinkedList implementation.
+
+It is not fixed size, here we can store heterogeneous elements.
+
+default capacity is 10. it is present in the list interface.
+
+elements will be store in the insertion order.
