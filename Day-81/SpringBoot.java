a16b714ed0d42f9a5321bdfd1338c0714ba2387a
firrushaik <shaikfirdose7799@gmail.com>
2025-12-13 21:58:41 +0530

    Spring Boot

---
+++ Day-81/SpringBoot.java
@@ -0,0 +1,3 @@
+Spring Boot is a Java framework built on top of Spring that simplifies application development. It eliminates boilerplate code with auto-configuration. Spring Boot comes with an embedded server, making applications production ready out of the box. It supports web apps, REST APIs, microservices, security and seamless cloud deployment.
+
+
