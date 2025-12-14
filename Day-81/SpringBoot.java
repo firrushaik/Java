683b6c03558188249b806d2048f7e4235f79fbbf
firrushaik <shaikfirdose7799@gmail.com>
2025-12-14 22:18:10 +0530

    Spring Boot

--- Day-81/SpringBoot.java
+++ Day-81/SpringBoot.java
@@ -1,3 +1,12 @@
-Spring Boot is a Java framework built on top of Spring that simplifies application development. It eliminates boilerplate code with auto-configuration. Spring Boot comes with an embedded server, making applications production ready out of the box. It supports web apps, REST APIs, microservices, security and seamless cloud deployment.
+Spring Boot is a Java framework built on top of Spring that simplifies application development. 
+It eliminates boilerplate code with auto-configuration. Spring Boot comes with an embedded server, making applications production ready out of the box.
+It supports web apps, REST APIs, microservices, security and seamless cloud deployment.
 
 
+Why learn Spring Boot?
+
+Fast Development: Quickly build production ready apps with minimal configuration.
+Auto-Configuration: Reduces boilerplate code, you can focus on business logic.
+Microservices Friendly: Perfect for building and managing distributed systems.
+Seamless Integrations: Works easily with databases, security, messaging and cloud platforms.
+Wide Adoption: Backed by Spring ecosystem and used in real world enterprise applications.
