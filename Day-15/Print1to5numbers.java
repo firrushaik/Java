91f8b25024eec90f376616c85335afe27df47cca
firrushaik <shaikfirdose7799@gmail.com>
2026-01-15 22:27:38 +0530

    Day-15 programs uploaded

--- Day-15/Print1to5numbers.java
+++ Day-15/Print1to5numbers.java
@@ -10,3 +10,19 @@ public class print1to5numbers {
 
     }
 }
+
+
+
+
+import java.util.Scanner;
+public class print1to5numbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the total number you have to print: ");
+        int num = sc.nextInt();
+        for(int i=1;i<=num;i++){
+            System.out.println(i);
+        }
+
+    }
+}
